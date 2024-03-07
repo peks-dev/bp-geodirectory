e8217d78f53c2658de4056de77e194eee557f81a
Peks <0peks0@proton.me>
2024-03-07 15:48:35 -0600

    onboarding folder created

---
+++ src/ui/onboardings/new-bp-onboarding.jsx
@@ -0,0 +1,22 @@
+import { RegisterBpIcon } from "@/ui/icons/new-bp-icon";
+import "./onboarding.css";
+
+const NewBpOnboarding = () => {
+  return (
+    <div className="onboarding">
+      <div className="onboarding__icon-container">
+        <RegisterBpIcon />
+      </div>
+      <h1 className="title title--xlg">basket place nuevo</h1>
+      <p>
+        Comparte tu cancha, crea encuentros memorables y forma parte de la
+        comunidad que impulsa el juego en cada rincón.
+      </p>
+      <button type="button" className="btn btn--primary">
+        agregar
+      </button>
+    </div>
+  );
+};
+
+export default NewBpOnboarding;
