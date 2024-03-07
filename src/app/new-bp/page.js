140617e2de54e2e23a8c6f78351e6a9347028f6d
Peks <0peks0@proton.me>
2024-03-07 15:46:43 -0600

    send code to onboarding component

--- src/app/new-bp/page.js
+++ src/app/new-bp/page.js
@@ -1,20 +1,14 @@
-import { RegisterBpIcon } from "@/ui/icons/new-bp-icon";
+"use client";
+import NewBpOnboarding from "@/ui/onboardings/new-bp-onboarding";
+import { useState } from "react";
 import "./page.css";
 
 export default function NewBp() {
+  const [start, setStart] = useState(false);
+
   return (
-    <section>
-      <div className="icon__container">
-        <RegisterBpIcon />
-      </div>
-      <h1>basket place nuevo</h1>
-      <p>
-        Comparte tu cancha, crea encuentros memorables y forma parte de la
-        comunidad que impulsa el juego en cada rincón.
-      </p>
-      <button type="button" className="btn btn--primary">
-        agregar
-      </button>
-    </section>
+    <>
+      <NewBpOnboarding />
+    </>
   );
 }
