ea2eaf1982b864692b8af1b96977e999da79ac3e
Peks <0peks0@proton.me>
2024-03-11 12:29:07 -0600

    match primary paths

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,13 @@ import { NextResponse } from 'next/server'
 export async function middleware(req) {
   const res = NextResponse.next()
 
+  console.log("ejecutando mdlw")
+
   // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient({ req, res })
 
   // Refresh session if expired - required for Server Components
-  await supabase.auth.getUser()
+  await supabase.auth.getSession()
 
   return res
 }
@@ -16,13 +18,6 @@ export async function middleware(req) {
 // Ensure the middleware is only called for relevant paths.
 export const config = {
     matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       * Feel free to modify this pattern to include more paths.
-       */
-      '/((?!_next/static|_next/image|favicon.ico).*)',
+      "/","/profile","new-bp","search"
     ],
   }
