7d6b7279660eb29e674846908da8af6988f15a86
Peks <0peks0@proton.me>
2024-03-11 16:50:39 -0600

    component created

---
+++ src/ui/court-card/court-card.jsx
@@ -0,0 +1,34 @@
+import Button from "@/ui/button/button";
+import Link from "next/link";
+import "./court-card.css";
+
+const CourtCard = ({ name, location, rating, level, id }) => {
+  return (
+    <article className="court-card">
+      <header className="court-card__header">
+        <picture className="court-card__img-container">
+          <img src="/test/photo-court.jpg" alt="court cover" />
+        </picture>
+        <div className="court-card__info-wrapper">
+          <div className="court-card__text-wrapper">
+            <h2 className="court-card__name">{name}</h2>
+            <div className="court-card__location">
+              <p>merida, yucatan</p>
+            </div>
+          </div>
+          <div className="court-card__rating">4</div>
+        </div>
+      </header>
+      <footer className="court-card__footer">
+        <p className="court-card__level">
+          nivel de juego: <span>{level}</span>
+        </p>
+        <Button variant={"secundary"}>
+          <Link href={`/courts/${id}`}>explorar</Link>
+        </Button>
+      </footer>
+    </article>
+  );
+};
+
+export default CourtCard;
