4d312078bb6e75b3101fa17fd95948b9247a73fb
Peks <0peks0@proton.me>
2024-03-11 18:02:38 -0600

    trying to fix deploy error 2

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -5,11 +5,17 @@ import AuthForm from "@/ui/forms/auth-form/auth-form";
 import ProfileWrapper from "@/ui/profile-wrapper/profile-wrapper";
 import "./page.css";
 
-export default async function Profile() {
+async function fetchSession() {
   const supabase = createServerActionClient({
     cookies,
   });
   const { session } = await getSession(supabase);
+
+  return session;
+}
+
+export default async function Profile() {
+  const session = await fetchSession();
 
   return (
     <section className="profile-page">
