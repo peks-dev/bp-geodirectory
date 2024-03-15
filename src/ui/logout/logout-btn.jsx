a449063f56989561ecfb64ccdcad6cb0c0481417
Peks <0peks0@proton.me>
2024-03-15 00:43:27 -0600

    reset stepForm global state added

--- src/ui/logout/logout-btn.jsx
+++ src/ui/logout/logout-btn.jsx
@@ -2,13 +2,16 @@
 import { useRouter } from "next/navigation";
 import { logout } from "@/services/supabase/auth.service";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useStepFormStore } from "@/store/court-store";
 import Button from "@/ui/btn/button";
 
 export const LogoutBtn = () => {
+  const { reset } = useStepFormStore();
   const supabase = createClientComponentClient();
   const router = useRouter();
   async function handleLogout() {
     try {
+      reset();
       await logout(supabase);
       router.refresh();
     } catch (error) {
