3a911cc70cdc3cc3043cdcc8c33e77a2c7624614
Peks <0peks0@proton.me>
2024-03-17 17:26:25 -0600

    geolocation function added

--- src/context/user-store.js
+++ src/context/user-store.js
@@ -1,45 +1,24 @@
-import { login, logout, register } from "@/services/supabase/auth.service";
-import { fetchDataOnTable } from "@/services/supabase/table-operations.service";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import UserModel from "@/models/user.model";
+import { getUserLocation } from "@/utilities/geolocation";
 
-const inicialState = new UserModel();
+const inicialState = {
+  location: null,
+};
 
 export const useUserStore = create(
   persist(
     (set) => ({
       ...inicialState,
-      loginUser: async (email, password) => {
+      getLocation: async () => {
         try {
-          const response = await login(email, password);
-          set((state) => ({
-            ...state,
-            id: response.user.id,
-            session: response.session,
-            email: response.user.email,
-          }));
-          const userData = await fetchDataOnTable(
-            "profiles",
-            "id",
-            response.user.id
-          );
-          set((state) => ({
-            ...state,
-            apodo: userData[0].apodo,
-            avatar_url: userData[0].avatar_url,
-            bio: userData[0].bio,
-            website: userData[0].website,
-          }));
+          const res = await getUserLocation();
+          set((state) => ({ ...state, location: res })); // Actualiza el estado con las coordenadas obtenidas
         } catch (error) {
-          throw error;
+          console.error("Error al obtener la ubicación del usuario:", error);
         }
       },
-      logOut: async () => {
-        await logout();
-        set(inicialState);
-      },
     }),
-    { name: "user-store", skipHydration: true }
+    { name: "user-store" }
   )
 );
