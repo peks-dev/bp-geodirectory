8d8e3b859c71ebf896bf074071ee683699de5d03
Peks <0peks0@proton.me>
2024-03-19 17:31:57 -0600

    changes

--- src/context/user-store.js
+++ src/context/user-store.js
@@ -3,17 +3,17 @@ import { persist } from "zustand/middleware";
 import { getUserLocation } from "@/utilities/geolocation";
 
 const inicialState = {
-  location: null,
+  userCoordinates: null,
 };
 
 export const useUserStore = create(
   persist(
     (set) => ({
       ...inicialState,
-      getLocation: async () => {
+      getUserCoordinates: async () => {
         try {
           const res = await getUserLocation();
-          set((state) => ({ ...state, location: res })); // Actualiza el estado con las coordenadas obtenidas
+          set((state) => ({ ...state, userCoordinates: res })); // Actualiza el estado con las coordenadas obtenidas
         } catch (error) {
           console.error("Error al obtener la ubicación del usuario:", error);
         }
