e5b5314dc1644ea33fec6e18e203e9281a67ecbc
Peks <0peks0@proton.me>
2024-03-19 17:34:17 -0600

    store change to context & name more descriptive

--- src/context/step-form-store.js
+++ src/context/step-form-store.js
@@ -1,27 +1,33 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { saveToLocalStorage } from "@/utilities/save-to-local-storage.utilitie";
+import CourtModel from "@/models/court.model";
+
+const blankStepFormData = new CourtModel();
 
 const initialState = {
   started: false,
   currentStep: 0,
-  location: null,
 };
 
 export const useStepFormStore = create(
   persist(
     (set) => ({
       ...initialState,
-      startedChange: () => set((state) => ({ started: !state.started })),
+      startedChange: () => {
+        saveToLocalStorage("stepFormData", blankStepFormData);
+        set((state) => ({ ...state, started: !state.started }));
+      },
       nextStep: (e) =>
         set((state) => {
           if (state.currentStep < 5) {
-            return { currentStep: state.currentStep + 1 };
+            return { ...state, currentStep: state.currentStep + 1 };
           }
         }),
       prevStep: (e) =>
         set((state) => {
           if (state.currentStep > 0) {
-            return { currentStep: state.currentStep - 1 };
+            return { ...state, currentStep: state.currentStep - 1 };
           }
         }),
       reset: () => {
