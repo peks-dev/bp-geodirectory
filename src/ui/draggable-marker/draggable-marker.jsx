e0a0dc9eae181383a433dcecb8653842a48a246f
Peks <0peks0@proton.me>
2024-03-19 17:38:38 -0600

    ajustments

---
+++ src/ui/draggable-marker/draggable-marker.jsx
@@ -0,0 +1,37 @@
+import L from "leaflet";
+import { Marker } from "react-leaflet";
+
+let theme = "light";
+
+const darkCourtMarkerIcon = "/svgs/court-marker.svg";
+
+const CourtMarkerIcon = L.icon({
+  iconUrl: darkCourtMarkerIcon,
+  iconRetinaUrl: darkCourtMarkerIcon,
+  iconAnchor: null,
+  shadowAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  iconSize: [24, 35],
+  className: "icono-masiso",
+});
+
+export const DraggableMarker = ({ markerPosition, onDragEnd }) => {
+  const handleDragEnd = (event) => {
+    const newPosition = event.target.getLatLng();
+    onDragEnd(newPosition);
+  };
+
+  return (
+    <Marker
+      draggable={true}
+      position={markerPosition}
+      icon={CourtMarkerIcon}
+      eventHandlers={{
+        dragend: handleDragEnd,
+      }}
+    />
+  );
+};
+
+export default DraggableMarker;
