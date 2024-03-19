e579c6d064fb118075670cfd3f56c54916cdaa8e
Peks <0peks0@proton.me>
2024-03-19 17:30:17 -0600

    utilitie created

---
+++ src/utilities/save-to-local-storage.utilitie.js
@@ -0,0 +1,8 @@
+export const saveToLocalStorage = (key, data) => {
+  try {
+    const serializedData = JSON.stringify(data);
+    localStorage.setItem(key, serializedData);
+  } catch (error) {
+    console.error("Error saving to local storage:", error);
+  }
+};
