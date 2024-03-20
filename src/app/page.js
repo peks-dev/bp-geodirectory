b4c877dd8091f64fef0f3a574408773dc49b0879
Peks <0peks0@proton.me>
2024-03-19 19:33:49 -0600

    app layout fixed for mobiles 2

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Map from "@/ui/map/Map";
 export default function MapPage() {
   const position = [51.505, -0.09];
   return (
-    <section className={styles.main}>
+    <section className={styles.mapWrapper}>
       <Map mapPosition={position} zoomLevel={13} />
     </section>
   );
