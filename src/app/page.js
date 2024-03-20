bf024163069833255caaa141768c42f08618b68a
Peks <0peks0@proton.me>
2024-03-19 23:51:07 -0600

    remove all from main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,9 @@
-"use client";
 import styles from "./page.module.css";
-import UserPositionMarker from "@/ui/user-marker/user-marker";
-import { useState } from "react";
-
-import dynamic from "next/dynamic";
-
-const DynamicMap = dynamic(() => import("../ui/map-leaflet/map-leaflet"), {
-  ssr: false,
-});
 
 export default function MapPage() {
-  const position = [51.505, -0.09];
-  const [mapCenter, setMapCenter] = useState(position);
   return (
     <section className={styles.mapWrapper}>
-      <DynamicMap coordinates={mapCenter} zoom={13}></DynamicMap>
+      <h1>aqui va el mapa</h1>
     </section>
   );
 }
