088388d6b5c3c19cdfae8c23264e3b2392936562
Peks <0peks0@proton.me>
2024-03-20 00:13:37 -0600

    map test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
+import dynamic from "next/dynamic";
 
 export default function MapPage() {
+  const DynamicMap = dynamic(() => import("../ui/map-leaflet/map-leaflet"), {
+    ssr: false,
+  });
+
+  const mapCenter = [52.0, 12.0];
+
   return (
     <section className={styles.mapWrapper}>
-      <h1>aqui va el mapa</h1>
+      <DynamicMap coordinates={mapCenter} zoom={13} />
     </section>
   );
 }
