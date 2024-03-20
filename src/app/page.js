db95a6cd4bebbc736ae3457d5df13773e515e75b
Peks <0peks0@proton.me>
2024-03-19 23:17:24 -0600

    dynamic rendering map

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
 "use client";
 import styles from "./page.module.css";
-import MapLeflet from "@/ui/map-leaflet/map-leaflet";
 import UserPositionMarker from "@/ui/user-marker/user-marker";
+import { useState } from "react";
+
+import dynamic from "next/dynamic";
+
+const DynamicMap = dynamic(() => import("../ui/map-leaflet/map-leaflet"), {
+  ssr: false,
+});
 
 export default function MapPage() {
   const position = [51.505, -0.09];
+  const [mapCenter, setMapCenter] = useState(position);
   return (
     <section className={styles.mapWrapper}>
-      <MapLeflet coordinates={position} zoom={13}>
-        <UserPositionMarker markerPosition={position} />
-      </MapLeflet>
+      <DynamicMap coordinates={mapCenter} zoom={13}>
+        <UserPositionMarker markerPosition={mapCenter} />
+      </DynamicMap>
     </section>
   );
 }
--- src/ui/map-leaflet/map-leaflet.jsx
+++ src/ui/map-leaflet/map-leaflet.jsx
@@ -1,24 +1,24 @@
 "use client";
 import "leaflet/dist/leaflet.css";
+import "./map-leaflet.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { openStreetMapTile } from "@/ui/map/lib/tile-layers";
 
 const MapLeflet = ({ coordinates, zoom, children }) => {
-  if (typeof window === "undefined") {
-    return null; // Retorna null en el lado del servidor
-  }
   return (
-    <MapContainer
-      center={coordinates}
-      zoom={zoom}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        attribution={openStreetMapTile.attribution}
-        url={openStreetMapTile.url}
-      />
-      {children}
-    </MapContainer>
+    <div className="map-wrapper">
+      <MapContainer
+        center={coordinates}
+        zoom={zoom}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution={openStreetMapTile.attribution}
+          url={openStreetMapTile.url}
+        />
+        {children}
+      </MapContainer>
+    </div>
   );
 };
 
