9c07930ed94fa77391a964f4015b25bb69a3864b
Peks <0peks0@proton.me>
2024-03-21 18:22:19 -0600

    clean page

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,43 +1,7 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import CourtCard from "@/ui/court-card/court-card";
-import { courtPreview } from "@/lib/court-preview";
-
-export const revalidate = 60;
-
 export default async function SearchPage() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
-
-  const { data } = await supabase.from("courts").select("*");
-  const courts = [];
-  await Promise.all(
-    data.map(async (court) => {
-      const newCourt = await courtPreview(court);
-      courts.push(newCourt);
-    })
-  );
-
-  if (!courts.length) {
-    return <div>no hay data</div>;
-  }
-
   return (
     <div>
       <h1>search page</h1>
-      <ul>
-        {courts.map((court) => {
-          return (
-            <li key={court.id}>
-              <CourtCard
-                name={court.name}
-                id={court.id}
-                level={court.game_level}
-              />
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
